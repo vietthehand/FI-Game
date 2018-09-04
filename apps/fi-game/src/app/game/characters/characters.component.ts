8b96d6cdd616f7cefa67664c2868290ab17dc74c
Viet Nguyen <vietthehand@yahoo.com>
2018-09-03 21:21:25 -0400

    character onpush to get rid of error

--- apps/fi-game/src/app/game/characters/characters.component.ts
+++ apps/fi-game/src/app/game/characters/characters.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'fi-characters',
   templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.scss']
+  styleUrls: ['./characters.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharactersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
