c0e2a1639a37bdc54f30311fbfd7934339351b9d
Viet Nguyen <vietthehand@yahoo.com>
2018-09-04 21:31:47 -0400

    adding moment to keep track of time

--- apps/fi-game/src/app/game/players/state/player.model.ts
+++ apps/fi-game/src/app/game/players/state/player.model.ts
@@ -1,15 +1,18 @@
 import { ID } from '@datorama/akita';
+import * as moment from 'moment';
 
 export interface Player {
   id: ID;
   job: string;
   money: number;
+  startDate: moment.Moment;
 }
 
 export function CreatePlayer() {
   return {
     id: 'test',
     job: '',
-    money: 0
+    money: 0,
+    startDate: moment()
   };
 }
